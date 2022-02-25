a754feacc15e6366d0facaff735bcc1fc3515c33
maashi <octtn8c@gmail.com>
2022-02-25 11:28:38 +0900

    fix: 見やすいように整理

--- src/js/assets/js/script.ts
+++ src/js/assets/js/script.ts
@@ -1,39 +1,42 @@
-import { hello } from '../../_module/_hello'
+const modal = () => {
+  const toggle = document.querySelectorAll('.toggle')
 
-hello('Hello World')
-
-const toggle = document.querySelectorAll('.toggle')
-console.log('aaa')
-
-toggle.forEach((element) => {
-  element.addEventListener('click', () => {
-    const id = element.getAttribute('data-modal')
-    const modal = document.querySelector(`#${id}`)
-    const state = Boolean(modal?.getAttribute('aria-hidden'))
-    modal?.setAttribute('aria-hidden', 'false')
-    console.log(modal)
-  })
-})
-
-const close = document.querySelectorAll('.close')
-
-close.forEach((element) => {
-  element.addEventListener('click', () => {
-    const modal = document.querySelectorAll('.modal')
-    modal.forEach((element) => {
-      element?.setAttribute('aria-hidden', 'true')
+  toggle.forEach((element) => {
+    element.addEventListener('click', () => {
+      const id = element.getAttribute('data-modal')
+      const modal = document.querySelector(`#${id}`)
+      modal?.setAttribute('aria-hidden', 'false')
     })
   })
-})
-const start = document.querySelectorAll('.start')
+}
 
-start.forEach((element) => {
-  element.addEventListener('click', () => {
-    const toast = document.querySelector('.toast')
-    console.log(toast)
-    toast?.classList.add('is-active')
-    setTimeout(() => {
-      toast?.classList.remove('is-active')
-    }, 6000)
+const closeModal = () => {
+  const close = document.querySelectorAll('.close')
+
+  close.forEach((element) => {
+    element.addEventListener('click', () => {
+      const modal = document.querySelectorAll('.modal')
+      modal.forEach((element) => {
+        element?.setAttribute('aria-hidden', 'true')
+      })
+    })
   })
-})
+}
+
+const toast = () => {
+  const start = document.querySelectorAll('.start')
+
+  start.forEach((element) => {
+    element.addEventListener('click', () => {
+      const toast = document.querySelector('.toast')
+      toast?.classList.add('is-active')
+      setTimeout(() => {
+        toast?.classList.remove('is-active')
+      }, 6000)
+    })
+  })
+}
+
+modal()
+closeModal()
+toast()
